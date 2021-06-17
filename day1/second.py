6e2851a1d6ea9912d535e1dcc454e9264f12b5ee
bhavesh1997 <bagnihotri12@gmail.com>
2021-06-17 19:09:49 +0530

    first

---
+++ day1/second.py
@@ -0,0 +1,8 @@
+for i in range(0,5):
+        for j in range(0,5):
+                if(i<=j):
+                        print(" ",end=" ")
+        for j in range(i*2-1):
+                
+                print("*",end=" ")
+        print("\n")
