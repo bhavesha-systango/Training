c9e750d51cc6efbde5916df967469041b12a7f66
bhavesha-systango <bagnihotri@isystango.com>
2021-06-24 15:35:57 +0530

    changes made

--- day5/inheritance.py
+++ day5/inheritance.py
@@ -1,8 +1,9 @@
 from class_and_object import Person
 
 class Student(Person):
-    def __init__(self, phone_no):
+    def __init__(self, name, age, nationality, phone_no):
         self.phone_no=phone_no
+        super().__init__(name, age, nationality)
 
     def get_phone_no(self):
         return self.phone_no
@@ -11,6 +12,7 @@ class Student(Person):
         return self
 
 
-s1 = Student(8878385967)
+s1 = Student("aryan",24,"indian",8878)
 print(s1.get_phone_no())
 print(s1.get_class())
+print(s1.greet())
