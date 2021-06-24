ee6338b65b7f9f29aa74be6840d15380fd2d741e
bhavesha-systango <bagnihotri@isystango.com>
2021-06-24 17:52:26 +0530

    changes done

--- day5/class_and_object.py
+++ day5/class_and_object.py
@@ -1,5 +1,5 @@
 class Person:
-    def __init__(self,name,age,nationality):
+    def __init__(self, name, age, nationality):
         self.name=name
         self.age=age
         self.nationality=nationality
