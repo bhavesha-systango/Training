aa13104745d77ddcf17ec21a9bea4dbcf78ac49a
bhavesha-systango <bagnihotri@isystango.com>
2021-07-04 21:01:48 +0530

    learned_csv

--- day6/salestax.py
+++ day6/salestax.py
@@ -1,33 +1,68 @@
-import csv
-import pandas
 import fs 
+import csv
+import json
+
+import pandas as pd
+
+class calculation_of_tax:
+
+    def read_File(self):
+        with open('day6/input1.csv', 'r') as file:
+            reader = csv.reader(file, delimiter = '\t')
+            for row in reader:
+                print(row)
+
+    def csv_to_json(self, csvFilePath, jsonFilePath):
+        jsonArray = []
+      
+        #read csv file
+        with open(csvFilePath, encoding='utf-8') as csvf: 
+            #load csv file data using csv library's dictionary reader
+            csvReader = csv.DictReader(csvf) 
+
+            #convert each csv row into python dict
+            for row in csvReader: 
+                #add this python dict to json array
+                jsonArray.append(row)
+  
+        #convert python jsonArray to JSON String and write to file
+        with open(jsonFilePath, 'w', encoding='utf-8') as jsonf: 
+            jsonString = json.dumps(jsonArray, indent=4)
+            jsonf.write(jsonString)
+          
+    def calculate_final_price(self):
+        with open('day6/input2.json') as json_file:
+            data = json.load(json_file)
+
+        indiatax = data[0]["tax"]
+        portuguesetax = data[1]["tax"]
+        spaintax = data[2]["tax"]
+
+        indiaProductCost = data[0]["ProductCost"]
+        portugueseProductCost = data[1]["ProductCost"]
+        spainProductCost = data[2]["ProductCost"]
+
+        df = pd.read_csv("day6/input1.csv")
+        ProductSalesTax = [indiatax, portuguesetax, spaintax]
+        df ['ProductSalesTax'] = ProductSalesTax
+        ProductFinalPrice = [int(indiaProductCost)+ int((int(indiaProductCost)*int(indiatax))/100),int(portugueseProductCost)+ int((int(portugueseProductCost)*int(portuguesetax))/100),int(spainProductCost)+ int((int(spainProductCost)*int(spaintax))/100)]
+        df ['ProductFinalPrice'] = ProductFinalPrice
+        return df
+
+    def output_to_file(self, df):
+        with open("day6/output10.csv", "w") as file:
+            dfd = repr(df)
+            file.write(dfd + "\n")
 
 
-csv_rowlist = [["ProductName","ProductCost","Country"], ["ponds",150,"india"],
-               ["boro-plus",200,"portuguese"],["himalaya",500,"spain"]]
-with open('users.csv', 'w') as file:
-    writer = csv.writer(file)
-    writer.writerows(csv_rowlist)
+if __name__=='__main__':
+    
+    c1 = calculation_of_tax()
+    csvFilePath = r'day6/input2.csv'
+    jsonFilePath = r'day6/input2.json'
+    c1.csv_to_json(csvFilePath, jsonFilePath)
+    c1.read_File()
+    data = c1.calculate_final_price()
+    c1.output_to_file(data)
 
-with open('users.csv', 'r') as file:
-    reader = csv.reader(file, delimiter = '\t')
-    for row in reader:
-        print(row)
-
-tax=10
-tax2=20
-tax3=30
-ProductCost = 150
-ProductCost2 = 200
-ProductCost3 = 500
-df = pandas.read_csv("users.csv")
-ProductSalesTax = [tax,tax2,tax3]
-df ['ProductSalesTax'] = ProductSalesTax
-ProductFinalPrice = [int(ProductCost)+ int((ProductCost*tax)/100),int(ProductCost2)+ int((ProductCost2*tax2)/100),int(ProductCost3)+ int((ProductCost3*tax3)/100)]
-df ['ProductFinalPrice'] = ProductFinalPrice
-print(df)
-
-
-with open("output10.csv", "w") as file:
-    dfd = repr(df)
-    file.write(dfd + "\n")
+      
